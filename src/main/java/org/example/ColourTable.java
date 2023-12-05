bf3a0831913f4336e9aeec9caf8ce5cf2388936b
hikmatb <hikmatbabalola@outlook.com>
2023-12-05 16:40:30 +0000

    Adding in docstrings

--- src/main/java/org/example/ColourTable.java
+++ src/main/java/org/example/ColourTable.java
@@ -6,6 +6,12 @@ public class ColourTable {
     int[][] colors;
     int size;
 
+    /**
+     * The ColourTable function is used to create a ColourTable object.
+     *
+     * @param paletteSize Set the size of the 2d array colors
+     *
+     */
     public ColourTable(int paletteSize) {
         if (!isValidPaletteSize(paletteSize)) {
             throw new IllegalArgumentException("Invalid palette size");
@@ -16,6 +22,15 @@ public class ColourTable {
         this.size = 0;
     }
 
+    /**
+     * The add function adds a color to the ColourTable.
+     *
+     * @param red Store the red value of a color
+     * @param green Store the green value of a color
+     * @param blue Store the blue value of a color
+     *
+     *
+     */
     public void add(int red, int green, int blue) {
         if (!isColorValueValid(red) || !isColorValueValid(green) || !isColorValueValid(blue)) {
             throw new IllegalArgumentException("Invalid color values");
@@ -37,18 +52,53 @@ public class ColourTable {
         size++;
     }
 
+    /**
+     * The getSize function returns the size of the array.
+     *
+     * @return The size of the stack
+     *
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * The isValidPaletteSize function checks if the given size is a valid palette size.
+     * A valid palette size must be greater than 1 and less than 1025, and it must also be a power of two.
+     *
+     * @param size Check if the size is a power of two
+     *
+     * @return True if the palette size is between 2 and 1024, inclusive
+     *
+     */
     private boolean isValidPaletteSize(int size) {
         return size > 1 && size < 1025 && (size & (size - 1)) == 0; // Check if it's a power of two
     }
 
+    /**
+     * The isColorValueValid function checks to see if the value passed in is a valid color value.
+     *
+     *
+     * @param value Determine whether the value is between 0 and 255
+     *
+     * @return A boolean value
+     *
+     */
     private boolean isColorValueValid(int value) {
         return value >= 0 && value <= 255;
     }
 
+    /**
+     * The containsColor function checks to see if the color is already in the array.
+     *
+     *
+     * @param red Check if the red value of a color is already in the array
+     * @param green Check if the green value of a color is already in the colors array
+     * @param blue Check if the color blue is in the array
+     *
+     * @return True if the color is already in the palette
+     *
+     */
     private boolean containsColor(int red, int green, int blue) {
         for (int i = 0; i < size; i++) {
             if (colors[i][0] == red && colors[i][1] == green && colors[i][2] == blue) {
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ColourTableTest {
 
+    /**
+     * The testValidPaletteSize function tests the ColourTable constructor to ensure that it throws an IllegalArgumentException when passed a palette size of 0, 1, or 1025.
+     * It also ensures that the constructor does not throw an exception when passed a valid palette size (2, 4, 8 or 512).
+     */
     @Test
     public void testValidPaletteSize() {
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(0));
@@ -15,6 +19,10 @@ public class ColourTableTest {
         assertDoesNotThrow(() -> new ColourTable(512));
     }
 
+    /**
+     * The testAddColor function tests the add function in ColourTable.java
+     * The testAddColor function checks if a valid color is added to the colour table, and if an invalid color is not added to the colour table.
+     */
     @Test
     public void testAddColor() {
         ColourTable colourTable = new ColourTable(4);
@@ -32,6 +40,10 @@ public class ColourTableTest {
         assertEquals(2, colourTable.getSize());
     }
 
+    /**
+     * The testExceedingCapacity function tests the add function in ColourTable.java to ensure that it throws an
+     * IllegalStateException when a colour is added to a table with no more space left.
+     */
     @Test
     public void testExceedingCapacity() {
         ColourTable colourTable = new ColourTable(2);
@@ -44,6 +56,13 @@ public class ColourTableTest {
         assertThrows(IllegalStateException.class, () -> colourTable.add(0, 0, 255));
     }
 
+    /**
+     * The testDuplicateColor function tests the add function in ColourTable.java to ensure that it throws an
+     * IllegalArgumentException when a duplicate colour is added to the table. The testDuplicateColor function first
+     * creates a new ColourTable object with 4 colours, then adds one colour (255, 0, 0) and checks that the size of
+     * the table is 1. Then it attempts to add another colour (255, 0, 0) and checks that an IllegalArgumentException is thrown
+     * and also ensures that there are still only 1 colours in the table after attempting to add a duplicate value. This test case passes if
+     */
     @Test
     public void testDuplicateColor() {
         ColourTable colourTable = new ColourTable(4);
